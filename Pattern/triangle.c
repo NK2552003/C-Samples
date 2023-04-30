26dcac7a2da1d6dd00178e63ee37887707d3f26e
Nitish Kumar <nk2552003@gmail.com>
2023-04-30 17:07:22 +0530

    pattern

--- Pattern/triangle.c
+++ Pattern/triangle.c
@@ -1,11 +1,12 @@
-//Making an equilateral triangle
+//This code is to make the rhombus 
+
 #include <stdio.h>
 int main(void)
 {
-    int r, c;
+    int r,c,a;
     for (r=1;r<=5;r++)
     {
-        int a=1;
+        a=1;
         for (c=1;c<=5;c++)
         {
             if ((r+c)>=6)
@@ -17,10 +18,38 @@ int main(void)
                 printf(" ");
             }
         }
-        a= a-2;
+        a=a-2;
         for (c=1;c<r;c++)
         {
             printf("%d",a--);
+        }
+        printf("\n");
+    }
+    for (r=1;r<=4;r++)
+    {
+        a=1;
+        for (c=1;c<=5;c++)
+        {
+            if (r>=c)
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("%d",a++);
+            }
+        }
+        a=a-2;
+        for (c=1;c<=4;c++)
+        {
+            if ((c+r)<5)
+            {
+                printf("%d",a--);
+            }
+            else
+            {
+                printf(" ");
+            }
         }
         printf("\n");
     }
