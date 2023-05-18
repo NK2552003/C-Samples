8e6701691fcec6c201986273df39ae73acd7b498
Nitish Kumar <nk2552003@gmail.com>
2023-05-18 22:33:57 +0530

    editing of practical 8

--- Practical_Work/pract_08.c
+++ Practical_Work/pract_08.c
@@ -1,56 +1,55 @@
-//WAP to bubble sort
+//make a program for sorting the arry the we inputed
 #include <stdio.h>
- 
-void swap(int* x, int* y)
+/*Function for swaping the values*/
+void swap(int* a, int* b)
 {
-    int temp = *x;
-    *x = *y;
-    *y = temp;
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
 }
- 
-// A function to implement bubble sort
-void bubbleSort(int arr[], int n)
+
+/*Function for bubble sort*/
+void bubblesort(int arr[], int size)
 {
-    int i, j;
-    for (i = 0; i < n - 1; i++)
+    int i,j;
+    for(i=0;i<size-1;i++)
     {
-        // Last i elements are already in place
-        for (j = 0; j < n - i - 1; j++)
+        for(j=0;j<size-i-1;j++)
         {
-            if (arr[j] > arr[j + 1])
+            if(arr[j]>arr[j+1])
             {
-                swap(&arr[j], &arr[j + 1]);
+                swap(&arr[j],&arr[j+1]);
             }
         }
     }
 }
- 
-/* Function to print an array */
-void printArray(int arr[], int size)
+
+/*Now making the function for output */
+
+void opt(int arr[],int size)
 {
     int i;
-    for (i = 0; i < size; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
-int main()
-{
-    int a,b,c,d,e,f,i;
-    printf("Enter the numbers in the array: ");
-    scanf("%d %d %d %d %d %d",&a,&b,&c,&d,&e,&f);
-    int arr[]={a,b,c,d,e,f};
-    int size = sizeof(arr)/sizeof(arr[0]);
-    printf("Size of array is %d \n", size);
-    printf("Entered array without sorting it is {");
+    printf("{");
     for (i=0;i<size;i++)
     {
         printf("%d, ",arr[i]);
     }
-    printf("}\n");
-    bubbleSort(arr, size);
-    printf("Sorted array: \n");
-    printArray(arr, size);
+    printf("}");
+}
+
+//main body of the file that we made 
+int main(void)
+{
+    int size,a,b,c,d,e,f;
+    printf("Enter 6 numbers in an array(enter the numbers by giving space between them)\n");
+    scanf("%d %d %d %d %d %d",&a,&b,&c,&d,&e,&f);
+    int arr[]={a,b,c,d,e,f};
+    /*Now let us find the size of an array that we made*/
+    size = sizeof(arr)/sizeof(arr[0]);
+    /*Now we have to make the function that will sort the array in ascending order*/
+    bubblesort(arr,size);
+    printf("Sorted array will be: ");
+    opt(arr,size);
     return 0;
 }
